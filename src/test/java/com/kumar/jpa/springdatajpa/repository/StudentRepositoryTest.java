3090c3d1a4b082c6eea37804105ddc1e9655cf2b
kumarsonu101 <kumar.sonu11@tcs.com>
2024-01-28 13:41:57 +0530

    Update test case

--- src/test/java/com/kumar/jpa/springdatajpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/kumar/jpa/springdatajpa/repository/StudentRepositoryTest.java
@@ -1,20 +1,55 @@
 package com.kumar.jpa.springdatajpa.repository;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.kumar.jpa.springdatajpa.entity.Guardian;
 import com.kumar.jpa.springdatajpa.entity.Student;
 
 @SpringBootTest
 public class StudentRepositoryTest {
 
-    void should_save_data_given_student_object() {
-        Student student = Student.builder()
-        .firstName("Sonu")
-        .lastName("kumar")
-        .email("sonuKumar@gmail.com")
-        .guardianName("XYZ")
-        .gurdianEmail("guardianEmail@gmail.com")
-        .guardianMobile("998877445566")
-        .build();
+    @Autowired
+    StudentRepository studentRepository;
+
+    Student student;
+    Guardian guardian;
+    Student studentWithGuardianDetails;
+
+    @BeforeEach
+    void return_student_object() {
+
+        guardian = Guardian.builder()
+                .name("ABC")
+                .email("newEmail@gmail.com")
+                .mobile("885585875692")
+                .build();
+
+                student = Student.builder()
+                .firstName("Sushant")
+                .lastName("kumar")
+                .email("sonuKumar@gmail.com")
+                .build();
+
+        studentWithGuardianDetails = Student.builder()
+                .firstName("Sonu")
+                .lastName("kumar")
+                .email("kumar@gmail.com")
+                .guardian(guardian)
+                .build();
+
     }
+
+    @Test
+    void should_save_data_given_student_object() {
+        studentRepository.save(student);
+    }
+
+    @Test
+    void should_save_student_with_guardian_data() {
+        studentRepository.save(studentWithGuardianDetails);
+    }
+
 }
